a4ee1fa3caa9f6c1c4267a647abe86691fdd27e7
Dyanara Dela Rosa <dyanaramdelarosa@gmail.com>
2024-12-02 19:24:34 +0800

    feat: Day 25 Project - US States Game

--- 025_us_states_game/US states game/main.py
+++ 025_us_states_game/US states game/main.py
@@ -3,33 +3,48 @@ import pandas as pd
 
 US_MAP = "blank_states_img.gif"
 ANSWERS = "50_states.csv"
+TOTAL_STATES = 50
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+screen.setup(width=725, height=491)
 screen.addshape(US_MAP)
 turtle.shape(US_MAP)
 
 states_data = pd.read_csv(ANSWERS)
-print(states_data)
 
 correct_guesses = 0
-while correct_guesses != 50:
+guessed_states = []
+
+while correct_guesses != TOTAL_STATES:
+    title = f"{correct_guesses}/{TOTAL_STATES} States Correct"
     answer_state = screen.textinput(
-        title="Guess the state",
-        prompt="What's another state's name?"
-    )
-    if states_data[states_data["state"] == answer_state]:
-        x_coor = states_data[states_data["state"] == answer_state]["x"]
-        y_coor = states_data[states_data["state"] == answer_state]["y"]
+        title=title, prompt="What's another state's name?"
+    ).title()
+
+    if answer_state == "Exit":
+        states_to_learn = states_data[~states_data.state.isin(guessed_states)].state
+        print(states_to_learn)
+        states_to_learn.to_csv("states_to_learn.csv", index=False)
+        break
+    correct_guess = states_data[states_data["state"] == answer_state]
+    if len(correct_guess) > 0:
+        # print(states_data[states_data["state"] == answer_state])
+        # x = states_data[states_data["state"] == answer_state].iloc[0]["x"]
+        # y = states_data[states_data["state"] == answer_state].iloc[0]["y"]
+        state_data = states_data[states_data["state"] == answer_state]
+        x = state_data.x.item()
+        y = state_data.y.item()
         correct_guesses += 1
+        guessed_states.append(answer_state)
         state = turtle.Turtle()
+        state.hideturtle()
         state.penup()
-        state.goto(x_coor, y_coor)
-        state.write()
+        state.goto(x, y)
+        state.write(answer_state, align="center")
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+if correct_guesses == 50:
+    print("You guessed them all!")
+    exit()
 
-turtle.onscreenclick(get_mouse_click_coor)
 turtle.mainloop()
-# screen.exitonclick()
